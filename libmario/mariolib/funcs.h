cab62b65bc12450dc42ece6ccbe9a33f1aa36acd
MelonSpeedruns <melonspeedruns@outlook.com>
2025-05-03 18:13:16 -0400

    Added possibility to set mario's default state in the reset_mario_state function (#1)

--- libmario/mariolib/funcs.h
+++ libmario/mariolib/funcs.h
@@ -181,4 +181,4 @@ void getMarioVelocity(f32 vel[3]);
 void setMarioVelocity(f32 pos[3]);
 void getMarioRotation(s16 rot[3]);
 void setMarioRotation(s16 rot[3]);
-void reset_mario_state(void);
+void reset_mario_state(u32 action);
